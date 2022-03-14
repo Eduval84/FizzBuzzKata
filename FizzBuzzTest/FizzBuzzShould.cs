21d8bd6a0f1f0abd8442d2840d9eb35df3cba300
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-14 23:29:25 +0000

    Refactor

--- FizzBuzzTest/FizzBuzzShould.cs
+++ FizzBuzzTest/FizzBuzzShould.cs
@@ -5,27 +5,34 @@ using Xunit;
 
 namespace FizzBuzzTest
 {
-    public class UnitTest1
+    public class FizzBuzzShould
     {
         [Fact]
-        public void Get1()
+        public void Get1AsString()
         {
             var result = FizzBuzz.Generate(1);
             Assert.Equal("1", result);
         }
 
         [Fact]
-        public void Get2()
+        public void Get2AsString()
         {
             var result = FizzBuzz.Generate(2);
             Assert.Equal("2",result);
         }
 
         [Fact]
-        public void GetFizz()
+        public void GetFizzFor3()
         {
             var result = FizzBuzz.Generate(3);
             Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void Get4AsString()
+        {
+            var result = FizzBuzz.Generate(4);
+            Assert.Equal("4", result);
         }
     }
 }
