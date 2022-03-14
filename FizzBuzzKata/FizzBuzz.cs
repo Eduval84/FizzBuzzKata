ea9d25919b56209bc341c2aff3795fa8c043fb52
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-14 23:48:55 +0000

    Green FizzBuzz Test for 15

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -10,6 +10,8 @@ namespace FizzBuzzKata
 
         public static string Generate(int num)
         {
+            if (num % 3 == 0 && num % 5 == 0)
+                return "FizzBuzz";
             if (num %3 == 0)
                 return "Fizz";
             if (num % 5 == 0)
